b2c1e223855230ce9a33e4f36dbf112328a01fb7
Oleg <olegkqa@gmail.com>
2017-11-21 00:57:07 +0600

    installed plugins for gulp: csso, stylus, load-plugins, created gulp task to make main.css from main.styl

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 'use strict';
 
 var gulp = require('gulp'),
-	pug = require('gulp-pug');
+	lp = require('gulp-load-plugins')();
 
 gulp.task('pug',function(){
 	return gulp.src('src/pug/pages/*.pug')
-		.pipe(pug({
+		.pipe(lp.pug({
 			pretty:true
 		}))
 		.pipe(gulp.dest('build'));
-})
+});
+
+gulp.task('stylus',function(){
+	return gulp.src('src/static/stylus/main.styl')
+		.pipe(lp.stylus({}))
+		.pipe(lp.csso())
+		.pipe(gulp.dest('build/static/css/'));
+});
