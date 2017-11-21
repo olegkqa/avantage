e4941d493a7a190124a6f8d5bb2d70fb2e36d98d
Oleg <olegkqa@gmail.com>
2017-11-21 13:35:00 +0600

    Installed plugins for gulp: notify, autoprefixer, sourcemaps. Configured default gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,26 @@ gulp.task('pug',function(){
 
 gulp.task('stylus',function(){
 	return gulp.src('src/static/stylus/main.styl')
+		.pipe(lp.sourcemaps.init())
 		.pipe(lp.stylus({}))
+		.pipe(lp.autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .on("error", lp.notify.onError({
+        message: "Error: <%= error.message %>",
+        title: "style"
+      	}))
 		.pipe(lp.csso())
+		.pipe(lp.sourcemaps.write())
 		.pipe(gulp.dest('build/static/css/'));
 });
+
+gulp.task('watch', function () {
+	gulp.watch('src/pug/**/*.pug',gulp.series('pug'));
+	gulp.watch('src/static/stylus/**/*.styl',gulp.series('stylus'))
+});
+
+gulp.task('default', gulp.series(
+	gulp.parallel('pug','stylus'),
+	'watch'
+	));
